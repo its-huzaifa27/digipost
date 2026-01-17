678a4ab67a3bb377f46d621d6f3620e48f206eea
MohammadM01 <mullamohammad123@gmail.com>
2026-01-17 15:21:57 +0530

    Dashboard fix

--- Frontend/src/components/layout/DashboardLayout.jsx
+++ Frontend/src/components/layout/DashboardLayout.jsx
@@ -3,12 +3,10 @@ import { Sidebar } from '../dashboard/Sidebar';
 
 export function DashboardLayout({ children }) {
     return (
-        <div className="flex min-h-screen bg-gray-50 font-sans">
+        <div className="min-h-screen bg-gray-50 font-sans">
             <Sidebar />
-            <main className="flex-1 ml-64 flex flex-col min-w-0">
-                <div className="w-full max-w-[1600px] mx-auto">
-                    {children}
-                </div>
+            <main className="flex-1 ml-32 flex flex-col min-w-0">
+                {children}
             </main>
         </div>
     );
